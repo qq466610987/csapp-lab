77c611f37839ab01b54828cfe9f2456dbd71e8eb
LM <466610987@qq.com>
2026-01-05 14:22:11 +0000

    1

--- datalab/datalab-handout/bits.c
+++ datalab/datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) { 
-  return ~(~x & ~y) & ~(x & y); 
+  return ~(~(x & ~y) & ~(~x & y));
 }
 /* 
  * tmin - return minimum two's complement integer 
